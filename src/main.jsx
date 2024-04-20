7a63ccf9028751361c3a98520e78a7ab591fa0aa
jasnmar <jasnmar@gmail.com>
2024-04-20 12:01:24 -0400

    Challenge Start

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Form from './Form.jsx'
+import App from './App'
+//import Form from './Form.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Form />
+    <App />
   </React.StrictMode>,
 )
